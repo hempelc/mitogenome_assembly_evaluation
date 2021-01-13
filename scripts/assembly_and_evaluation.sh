ad38979600aaa99b87602cabc4ee5f5a8677d36a
hempelc <hempelc@uoguelph.ca>
2021-01-13 10:18:23 -0500

    Added parameter -c and fixed some bugs

--- scripts/assembly_and_evaluation.sh
+++ scripts/assembly_and_evaluation.sh
@@ -10,25 +10,27 @@
 # run the following script as follows: source mitogenome_assembly_evaluation_installations.sh
 
 cmd="$0 $@" # Make variable containing full used command to print command in logfile
-usage="$(basename "$0") -1 <R1.fastq> -2 <R2.fastq> -l <length> [-t <n>]
+usage="$(basename "$0") -1 <R1.fastq> -2 <R2.fastq> -l <length> -c <clade> [-t <n>]
 
 Usage:
 	-1  Reads1
 	-2  Reads2
 	-l  Read length (needed for Mitoz)
-  -t  Threads (default:16)
+  -c  Clade of species ('Arthropoda' or 'Chordata')
+  -t  Threads (default: 16)
 	-h  Display this help and exit"
 
 # Set default options:
 threads='16'
 
 # Set specified options
-while getopts ':1:2:l:t:h' opt; do
+while getopts ':1:2:l::ct:h' opt; do
   case "${opt}" in
     1) R1="${OPTARG}" ;;
     2) R2="${OPTARG}" ;;
     l) length="${OPTARG}" ;;
-		t) threads="${OPTARG}" ;;
+    c) clade="${OPTARG}" ;;
+    t) threads="${OPTARG}" ;;
 		h) echo "${usage}"
        exit ;;
     :) printf "Option -$OPTARG requires an argument."
@@ -42,14 +44,27 @@ done
 shift $((OPTIND - 1))
 
 # Check if required options are set
-if [[  -z "${R1}" || -z "${R2}" || -z "${length}" ]]
+if [[  -z "${R1}" || -z "${R2}" || -z "${length}" || -z "${clade}"]]
 then
-   echo -e "\n-1, -2, and -l must be set.\n"
+   echo -e "\n-1, -2, -l, and -c must be set.\n"
    echo -e "${usage}\n\n"
    echo -e "Exiting script.\n"
    exit
 fi
 
+if [[ ${clade} != 'Arthropoda' && ${clade} != 'Chordata' ]]; then
+  echo -e "Invalid option for -c, must be set to either 'Arthropoda' or 'Chordata'\n"
+  echo -e "$usage\n\n"
+  echo -e "Exiting script\n"
+  exit
+fi
+
+## Set genetic code based on clade
+if [[ ${clade} == 'Arthropoda' ]]; then
+  genetic_code='5'
+else
+  genetic_code='2'
+fi
 
 # Define function to print steps with time
 start=$(date +%s)
@@ -66,6 +81,7 @@ step_description_and_time "OPTIONS"
 echo -e "Forward reads were defined as ${R1}.\n"
 echo -e "Reverse reads were defined as ${R2}.\n"
 echo -e "Read length was defined as ${length}.\n"
+echo -e "Number of threads was set to ${threads}.\n"
 echo -e "Number of threads was set to ${threads}.\n"
 echo -e "Script started with full command: ${cmd}\n"
 
@@ -110,7 +126,7 @@ step_description_and_time "Running MitoZ assembly module"
 MitoZ.simg assemble \
 --genetic_code 5 \
 --clade Arthropoda \
---outprefix MitoZ \
+--outprefix MITOZ_ASSEMBLY \
 --thread_number ${threads} \
 --fastq1 ../${R1} \
 --fastq2 ../${R2} \
@@ -119,16 +135,16 @@ MitoZ.simg assemble \
 --run_mode 2 \
 --filter_taxa_method 1 \
 --requiring_taxa 'Arthropoda'
-mv tmp MitoZ.result/
-mv MitoZ.result/ MITOZ/
+mv tmp/ tmp_assembly/
+mv tmp_assembly/ MITOZ_ASSEMBLY.result/
 ### Rename output files
-for i in MITOZ/work71.*; do
+for i in MITOZ_ASSEMBLY.result/work71.*; do
   mv ${i} $(echo ${i} | sed 's/work71/mitoz/')
 done
 
 # Make list of files from assemblers (all but MitoZ)
 assembly_list=(MEGAHIT/final.contigs.fa SPADES/scaffolds.fasta RNASPADES/scaffolds.fasta IDBA_UD/contig.fa IDBA_TRAN/contig.fa \
-TRINITY/Trinity.fa MITOZ/mitoz.mitogenome.fa)
+TRINITY/Trinity.fa MITOZ_ASSEMBLY.result/mitoz.mitogenome.fa)
 
 # Running the MitoZ modules findmitoscaf and annotate on all assembly outputs
 for i in ${assembly_list}; do
@@ -140,27 +156,29 @@ for i in ${assembly_list}; do
   ## Findmitoscaf module
   step_description_and_time "Running MitoZ findmitoscaf module on ${assembler} output"
 	MitoZ.simg findmitoscaf \
-	--genetic_code 5 \
-	--clade Arthropoda \
+	--genetic_code ${genetic_code} \
+	--clade ${clade} \
 	--outprefix ${assembler}_findmitoscaf \
 	--thread_number ${threads} \
 	--fastq1 ../${R1} \
 	--fastq2 ../${R2} \
 	--fastq_read_length ${length} \
 	--fastafile ${assembly_result}
-  mv tmp ${assembler}_findmitoscaf.result
+  mv tmp/ tmp_finsmitoscaf/
+  mv tmp_finsmitoscaf/ ${assembler}_findmitoscaf.result
 
 	## Annotate module
-  step_description_and_time "Running MitoZ annotate module on ${assembler}"
+  step_description_and_time "Running MitoZ annotate module on ${assembler} output"
   MitoZ.simg annotate \
-	--genetic_code 5 \
-	--clade Arthropoda \
+	--genetic_code ${genetic_code} \
+	--clade ${clade} \
 	--outprefix ${assembler}_annotate \
 	--thread_number ${threads} \
 	--fastq1 ../${R1} \
 	--fastq2 ../${R2} \
 	--fastafile ${assembler}_findmitoscaf.result/${assembler}_findmitoscaf.mitogenome.fa
-	mv tmp ${assembler}_annotate.result
+	mv tmp/ tmp_annotate/
+  mv tmp_annotate/ ${assembler}_annotate.result
   cd ..
 done
 
