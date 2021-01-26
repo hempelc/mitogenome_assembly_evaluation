02f9e02482fd1b7c45afcea563f9bb07a5f0f287
hempelc <hempelc@uoguelph.ca>
2021-01-26 12:28:29 -0500

    Added master summary evaluation, keeping mitogenome scaffolds, and fixes

--- scripts/assembly_and_evaluation.sh
+++ scripts/assembly_and_evaluation.sh
@@ -9,8 +9,10 @@
 # To download/install all programs and add them to the PATH automatically,
 # run the following script as follows: source mitogenome_assembly_evaluation_installations.sh
 
+# TO DO: save actual scaffold .fa files
+
 cmd="$0 $@" # Make variable containing full used command to print command in logfile
-usage="$(basename "$0") -1 <R1.fastq> -2 <R2.fastq> -l <length> -c <clade> [-t <n> -ae]
+usage="$(basename "$0") -1 <R1.fastq> -2 <R2.fastq> -l <length> -c <clade> [-t <n> -aes]
 
 Usage:
   -1  Reads1
@@ -19,6 +21,7 @@ Usage:
   -c  Clade of species ('Arthropoda' or 'Chordata')
   -a  Assemblies only
   -e  Evaluation only
+  -s  Shut down machine after script is done (to minimize costs for AWS)
   -t  Threads (default: 16)
   -h  Display this help and exit"
 
@@ -26,9 +29,10 @@ Usage:
 threads='16'
 asmbl_flag=true
 eval_flag=true
+shutdown=false
 
 # Set specified options
-while getopts ':1:2:l:c:aet:h' opt; do
+while getopts ':1:2:l:c:aest:h' opt; do
   case "${opt}" in
     1) R1=$(realpath "${OPTARG}") ;;
     2) R2=$(realpath "${OPTARG}") ;;
@@ -36,6 +40,7 @@ while getopts ':1:2:l:c:aet:h' opt; do
     c) clade="${OPTARG}" ;;
     a) eval_flag=false ;;
     e) asmbl_flag=false ;;
+    s) shutdown=true ;;
     t) threads="${OPTARG}" ;;
 		h) echo "${usage}"
        exit ;;
@@ -77,6 +82,17 @@ step_description_and_time () {
 	echo -e "\n======== [$(date +%H:%M:%S)] ${1} [Runtime: $((($(date +%s)-$start)/3600))h $(((($(date +%s)-$start)%3600)/60))m] ========\n" #" adding outcommented quote here to fix bug in colouring scheme of personal text editor
 }
 
+# Define function to get the scaffold info that's at the top of every MitoZ summary file
+get_scaffold_info () {
+  sed "/--/q" "${1}" | head -n -4 | tail -n +2
+}
+
+# Define function to get the closely related species info from the scaffold info
+get_spec_name () {
+  get_scaffold_info "${1}" | grep -Po '[A-Za-z]* [a-z]* *$'
+}
+
+
 ( # Bracket for log file
 
 
@@ -86,7 +102,7 @@ step_description_and_time "OPTIONS"
 echo -e "Forward reads were defined as ${R1}.\n"
 echo -e "Reverse reads were defined as ${R2}.\n"
 echo -e "Read length was defined as ${length}.\n"
-echo -e "Clade was set to ${clade} with genetic code ${genetic_code}.\n".
+echo -e "Clade was set to ${clade} with genetic code ${genetic_code}.\n"
 echo -e "Number of threads was set to ${threads}.\n"
 echo -e "Script started with full command: ${cmd}\n"
 
@@ -155,24 +171,31 @@ if [[ "${asmbl_flag}" == "true" ]]; then
   --run_mode 2 \
   --filter_taxa_method 1 \
   --requiring_taxa 'Arthropoda'
-  mv tmp/ MITOZ_ASSEMBLY.result/assembly_tmp/
+  mv MITOZ_ASSEMBLY.result/ MITOZ/
+  mv tmp/ MITOZ/assembly_tmp/
   ### Rename output files
-  for i in MITOZ_ASSEMBLY.result/work71.*; do
+  for i in MITOZ/work71.*; do
     mv ${i} $(echo ${i} | sed 's/work71/mitoz/')
   done
-mv MITOZ_ASSEMBLY.result/mitoz.mitogenome.fa assemblies/
-rm -r MITOZ_ASSEMBLY.result/ # remove to save space
+mv MITOZ/mitoz.mitogenome.fa assemblies/
+rm -r MITOZ/ # remove to save space
 fi
 
 if [[ "${eval_flag}" == "true" ]]; then
   # Make list of files from assemblers (all but MitoZ)
-  assembly_list=(MEGAHIT/final.contigs.fa SPADES/scaffolds.fasta RNASPADES/transcripts.fasta IDBA_UD/contig_IDBA_UD.fa IDBA_TRAN/contig_IDBA_TRAN.fa \
-  TRINITY/Trinity.fasta MITOZ_ASSEMBLY.result/mitoz.mitogenome.fa)
+  assembly_list=(MEGAHIT/final.contigs.fa SPADES/scaffolds.fasta RNASPADES/transcripts.fasta \
+  IDBA_UD/contig_IDBA_UD.fa IDBA_TRAN/contig_IDBA_TRAN.fa TRINITY/Trinity.fasta \
+  MITOZ/mitoz.mitogenome.fa)
 
   # Running the MitoZ modules findmitoscaf and annotate on all assembly outputs
   mkdir -p evaluation/
   mkdir -p evaluation/circos/
   mkdir -p evaluation/summaries/
+  mkidr -p evaluation/mitogenome_scaffolds/
+
+  # Make master file for master summary output
+  echo -e "Assembler\tScaffolds\tCircular\tClosely related species\tProtein coding genes\ttRNA genes\trRNA genes\tTotal genes\tMissing genes" \
+  > evaluation/summaries/master_summary.txt
 
   for i in "${assembly_list[@]}"; do
     ## Make variables for easier file handling and change into respective dir
@@ -180,7 +203,7 @@ if [[ "${eval_flag}" == "true" ]]; then
     assembly_result=$(realpath assemblies/$(basename ${i}))
     cd evaluation/
 
-    ## Make fasta headers short, otherwise errors
+    ## Make fasta headers shorter for some assemblers, otherwise errors
     if [[ ${assembler} == "TRINITY" ]]; then
       echo "Shortening ${assembler} fasta head names"
       awk '{for(x=1;x<=NF;x++)if($x~/TRINITY/){sub(/TRINITY/,++i)}}1' \
@@ -223,19 +246,79 @@ if [[ "${eval_flag}" == "true" ]]; then
   	--fastq2 ${R2} \
   	--fastafile ${assembler}_findmitoscaf.result/${assembler}_findmitoscaf.mitogenome.fa
   	rm -r tmp/
+
     ## Rename result files
     for i in circos.png circos.svg summary.txt; do
       mv ${assembler}_annotate.result/${i} ${assembler}_annotate.result/${assembler}_${i}
     done
+
     ## Copy result files and remove folders to save space
     cp ${assembler}_annotate.result/*circos* circos/
     cp ${assembler}_annotate.result/*summary.txt summaries/
+    cp ${assembler}_annotate.result/*.fasta mitogenome_scaffolds/
     rm -r ${assembler}_*.result/
+
+    ## Extract info from MitoZ summary
+    ### Note that to get info about specific parts of the file, we use hardcoded
+    ### integers to delet certain lines, which would have to be adapted if file
+    ### format changes
+
+    ###Define summary file
+    summary_file="summaries/${assembler}_summary.txt"
+
+    ### Get number of scaffolds
+    scaf_num=$(get_scaffold_info "${summary_file}" | wc -l)
+
+    ### Test if scaffolds are circular
+    if [[ "${scaf_num}" == 1 ]]; then
+      if [[ $(get_scaffold_info "${summary_file}" | grep "yes") ]]; then
+        circ="yes"
+      else
+        circ="no"
+      fi
+    else
+      circ="NA"
+    fi
+
+    ### Check if only one closely related species shared between scaffolds
+    #### VAR stands for varying, as in not uniform between scaffolds
+    if [[ $(get_spec_name "${summary_file}" | uniq | wc -l) == 1 ]]; then
+      spec=$(get_spec_name "${summary_file}" | uniq | cut -f 1-2 -d ' ')
+    elif [[ $(get_spec_name "${summary_file}" | cut -f 1 -d ' ' | uniq | wc -l) == 1 ]]; then
+      spec=$(echo "$(get_spec_name "${summary_file}" | cut -f 1 -d ' ' | uniq) VAR")
+    else
+      spec="VAR"
+    fi
+
+    ### Get info about found and missing genes:
+    pcg=$(grep "Protein coding genes totally found" "${summary_file}" | cut -f 2 -d ":" | sed 's/ //g')
+    trna=$(grep "tRNA genes totally found" "${summary_file}" | cut -f 2 -d ":" | sed 's/ //g')
+    rrna=$(grep "rRNA genes totally found" "${summary_file}" | cut -f 2 -d ":" | sed 's/ //g')
+    total=$(grep "Genes totally found" "${summary_file}" | cut -f 2 -d ":" | sed 's/ //g')
+    if [[ $(sed -n '/Potential missing genes/,$p' "${summary_file}") ]]; then
+      miss=$(sed -n '/Potential missing genes/,$p' "${summary_file}" | head -n -5 \
+      | tail -n +4 | sed 's/^[^ ]* *//g' | awk '{s+=$1} END {print s}')
+    else
+      miss=0
+    fi
+
+    ### Save all info into one variable
+    summary=$(echo -e "${assembler}\t${scaf_num}\t${circ}\t${spec}\t${pcg}\t${trna}\t${rrna}\t${total}\t${miss}")
+
+    ### Attach info to master file
+    echo "${summary}" >> summaries/master_summary.txt
+
     cd ..
   done
 fi
 
 cd ..
 
+# Make log file and move it into dir
 ) 2>&1 | tee mitogenome_assembly_log.txt # Make logfile
 mv mitogenome_assembly_log.txt mitogenome_assembly_evaluation_results/
+
+# Shut down machine if wanted
+if [[ "${shutdown}" == "true" ]]; then
+  sudo shutdown now -h
+fi
